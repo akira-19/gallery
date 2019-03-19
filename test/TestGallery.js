f80b4962c8af6788342f02f0465cbdd1b2095ace
akira-19 <dounatsu00@yahoo.co.jp>
2019-03-18 23:56:41 -0700

    modified the test

--- test/TestGallery.js
+++ test/TestGallery.js
@@ -2,33 +2,33 @@ var Gallery = artifacts.require("Gallery");
 const truffleAssert = require('truffle-assertions');
 
 contract("GalleryTest", function(accounts){
-    // it("should make the creator and contract address the same", function(){
-    //     var galleryInstance;
-    //     return Gallery.new({from: accounts[0]}).then(function(instance){
-    //         galleryInstance = instance;
-    //         return galleryInstance.createToken();
-    //     }).then((result) => {
-    //         truffleAssert.eventEmitted(result, 'Creators', (ev) => {
-    //             return ev.creator == result.logs[0].address;
-    //         }, 'Creator should be contract address');
-    //     }).catch(err => {
-    //         console.log(err);
-    //     })
-    // });
-    //
-    // it("should have 1000 tokens at first", function(){
-    //     var galleryInstance;
-    //     return Gallery.new({from: accounts[0]}).then(function(instance){
-    //         galleryInstance = instance;
-    //         return galleryInstance.createToken();
-    //     }).then(() => {
-    //         return galleryInstance.mintToken();
-    //     }).then((result) => {
-    //         assert.equal(result.logs[0].args[4], 1000, "the contract should have 1000 token");
-    //     }).catch(err => {
-    //         console.log(err);
-    //     })
-    // })
+    it("should make the creator and contract address the same", function(){
+        var galleryInstance;
+        return Gallery.new({from: accounts[0]}).then(function(instance){
+            galleryInstance = instance;
+            return galleryInstance.createToken();
+        }).then((result) => {
+            truffleAssert.eventEmitted(result, 'Creators', (ev) => {
+                return ev.creator == result.logs[0].address;
+            }, 'Creator should be contract address');
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    it("should have 1000 tokens at first", function(){
+        var galleryInstance;
+        return Gallery.new({from: accounts[0]}).then(function(instance){
+            galleryInstance = instance;
+            return galleryInstance.createToken();
+        }).then(() => {
+            return galleryInstance.mintToken();
+        }).then((result) => {
+            assert.equal(result.logs[0].args[4], 1000, "the contract should have 1000 token");
+        }).catch(err => {
+            console.log(err);
+        })
+    })
 
     it("should have NFT", function(){
         let galleryInstance;
@@ -43,14 +43,6 @@ contract("GalleryTest", function(accounts){
             tokenId = tokenType;
             galleryInstance.mintNonFungible(tokenType, accounts[0]);
         }).then(() => {
-            //
-            // console.log('korega');
-            // console.log(typeof tokenId.logs[0].args[3]);
-            // console.log('korega');
-            //
-            console.log(tokenId.logs[0].args[3]);
-
-            // tokenId = web3.utils.toBN(tokenId.logs[0].args[3]);
             return galleryInstance.balanceOf(accounts[0],tokenId.logs[0].args[3]);
         }).then( balance => {
             // console.log(balance);
